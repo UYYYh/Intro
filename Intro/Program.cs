eb7338368d2cfe69b3a63d96126a4d607ec14a72
Kevin Wang <Kevin Wang@LAPTOP-J2S3GGFK>
2022-09-09 12:34:49 +0100

    Added Volume function.

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Calculating the volume of the fish tank
+using System;
+
+namespace Intro_Program
+{
+    class Program
+    {
+        static double Vol(double length, double height, double width)
+        {
+            return length * height * width;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Vol(10, 20, 10));
+    
+        }
+    }
+}
